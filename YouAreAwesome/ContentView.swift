e2c6b5f0b53d397c9f24fdf7b39c9b23aea04885
CT Reatherford <taoist22@gmail.com>
2025-06-28 16:38:31 -1000

    added buttons, HStack and frame, other design elements

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -11,17 +11,37 @@ struct ContentView: View {
     @State private var message = "I am a Programmer!"
     var body: some View {
         VStack {
+            
+            Spacer()
+            
             Image(systemName: "swift")
                 .resizable()
                 .scaledToFit()
                 .foregroundStyle(.orange)
+                .frame(width: 200, height: 200)
+            
             Text(message)
                 .font(.largeTitle)
-                .fontWeight(.heavy)
-                .foregroundStyle(.red)
-            Button("Click Me!") {
-                message = "Awesome!"
+                .fontWeight(.ultraLight)
+                
+            Spacer()
+            
+            HStack {
+                Button("Awesome!") {
+                    message = "Awesome!"
+                }
+                
+                Button("Great!") {
+                    message = "Great"
+                }
+                
             }
+            .buttonStyle(.borderedProminent)
+            .font(.title2)
+            .tint(.orange)
+            
+            
+            
         }
         .padding()
     }
