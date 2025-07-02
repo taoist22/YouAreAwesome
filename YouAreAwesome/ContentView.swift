8f99b3d0ec43a9c905e0cd2384760fcab17e4b74
CT Reatherford <taoist22@gmail.com>
2025-07-01 15:34:54 -1000

    Add mutliple images and messages to app, implement logic for cycle count.

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -11,6 +11,7 @@ struct ContentView: View {
     @State private var message = ""
     @State private var imageName = ""
     @State private var imageNumber = 0
+    @State private var messageNumber = 0
     var body: some View {
         VStack {
             
@@ -26,14 +27,24 @@ struct ContentView: View {
                 .font(.largeTitle)
                 .fontWeight(.heavy)
                 .foregroundStyle(.red)
+                .multilineTextAlignment(.center)
             
             Spacer()
             
             Button("Show Message!") {
-                let message1 = "You are Awesome!"
-                let message2 = "You are Great!"
+                let messages = ["You Are Awesome!",
+                                "You Are Great!",
+                                "You Are Fantastic!",
+                                "Fabulous? That's You!",
+                                "You Make Me Smile!",
+                                "When the Genius Bar Needs Help, They Call You!"]
                 
-                message = (message == message1 ? message2: message1)
+                message = messages[messageNumber]
+                messageNumber += 1
+                if messageNumber == messages.count {
+                    messageNumber = 0
+                }
+                
 
                 imageName = "image\(imageNumber)"
                 imageNumber += 1
