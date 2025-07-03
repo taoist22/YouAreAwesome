b5fe44b9d0f4a691c6a293d54d0b840d10093319
CT Reatherford <taoist22@gmail.com>
2025-07-02 16:13:07 -1000

    Change text message location, added frame for text message, added animation to image and text.

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -14,30 +14,33 @@ struct ContentView: View {
     @State private var messageNumber = 0
     var body: some View {
         VStack {
-            
-            Spacer()
+            Text(message)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .foregroundStyle(.red)
+                .multilineTextAlignment(.center)
+                .minimumScaleFactor(0.5)
+                .frame(height: 130)
+                .animation(.easeInOut(duration: 0.15), value: message)
             
             Image(imageName)
                 .resizable()
                 .scaledToFit()
                 .clipShape(RoundedRectangle(cornerRadius: 30))
                 .shadow(radius: 30)
+                .animation(.default, value: imageName)
             
-            Text(message)
-                .font(.largeTitle)
-                .fontWeight(.heavy)
-                .foregroundStyle(.red)
-                .multilineTextAlignment(.center)
+            
             
             Spacer()
             
             Button("Show Message!") {
                 let messages = ["You Are Awesome!",
+                                "When the Genius Bar Needs Help, They Call You!",
                                 "You Are Great!",
                                 "You Are Fantastic!",
                                 "Fabulous? That's You!",
-                                "You Make Me Smile!",
-                                "When the Genius Bar Needs Help, They Call You!"]
+                                "You Make Me Smile!",]
                 
                 message = messages[messageNumber]
                 messageNumber += 1
